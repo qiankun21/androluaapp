4e781853cf7ae41f700877b7b4a4cc34f45059f4
qiankun21 <102011436+qiankun21@users.noreply.github.com>
2025-01-16 12:28:40 +0800

    创建端口

---
+++ proxy.ts
@@ -0,0 +1,19 @@
+export default async function handler(req: Request): Promise<Response> {
+  const url = new URL(req.url);
+  const targetUrl = `http://open.sc198.cc${url.pathname}${url.search}`;
+
+  const response = await fetch(targetUrl, {
+    method: req.method,
+    headers: req.headers,
+    body: req.body,
+  });
+
+  const headers = new Headers(response.headers);
+  headers.set("X-Proxy-By", "Deno Proxy Example");
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers,
+  });
+}
